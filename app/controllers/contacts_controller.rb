85f80c266cb7bc4c57ae759d2da24a504ca6d499
kengo <zsensikengo@gmail.com>
2021-05-01 18:32:13 +0900

    コントローラーの設定

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,20 @@
 class ContactsController < ApplicationController
-  def index
+  def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.send_mail(@contact).deliver_now
+    else
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone_number, :message)
   end
   
 end
