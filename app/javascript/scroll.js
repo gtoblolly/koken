13bf8edee1880996b19c2764738663629c09f7fa
kengo <zsensikengo@gmail.com>
2021-05-02 17:46:01 +0900

    会社案内スクロール機能実装

--- app/javascript/scroll.js
+++ app/javascript/scroll.js
@@ -1,3 +1,19 @@
-window.addEventListener('load', () => {
-  console.log("OK");
+window.addEventListener('turbolinks:load', () => {
+const guide = document.getElementById("guide")
+const project = document.getElementById("project")
+const instance = document.getElementById("instance")
+const news = document.getElementById("news")
+
+guide.addEventListener('click', () => {
+ const TargetGuide = document.getElementById("target_guide")
+ const rect = TargetGuide.getBoundingClientRect().top;
+ const offset = window.pageYOffset;
+ const gap = 50;
+ const target = rect + offset - gap;
+ window.scrollTo({
+  top: target,
+  behavior: "smooth"
+});
+});
+
 });
