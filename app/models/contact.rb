3815fdd43f4344d30a86eeacb7396a1318e5b217
kengo <zsensikengo@gmail.com>
2021-05-14 06:32:30 +0900

    バリデーションの設定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :phone_number
+    validates :message
+  end
+
+  validates :area, inclusion: { in: ["東京都", "神奈川県", "埼玉県", "千葉県", "その他"] }
+  validates :contents, inclusion: { in: ["戸建て", "マンション", "テナント工事", "現状回復"] }
 end
